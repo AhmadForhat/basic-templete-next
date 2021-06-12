dcc3bf626e573f271a4b2a4dd24e8d1e6181fb21
guts <wiliam.tae@gmail.com>
2021-06-12 18:29:53 -0300

    refactor: change global styles import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 import { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
 
-import GlobalStyles from '../styles/global'
+import { GlobalStyle } from '../styles/global'
 import theme from '../styles/theme'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => (
   <ThemeProvider theme={theme}>
+    <GlobalStyle />
     <Component {...pageProps} />
-    <GlobalStyles />
   </ThemeProvider>
 )
 
