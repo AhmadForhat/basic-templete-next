b38d81e6c9e43b01cb63d2d17b2c29040dd90b9f
AhmadForhat <ahmad@evnts.com.br>
2021-08-21 16:19:33 -0300

    fix: :bug: problema de body resolvido

--- src/pages/api/lead.ts
+++ src/pages/api/lead.ts
@@ -1,19 +1,29 @@
 import axios from 'axios'
 import { NextApiRequest, NextApiResponse } from 'next'
 
+const token = 'Bearer 0586907c4f941f17ceca3742d85b94e1'
+
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export default (request: NextApiRequest, response: NextApiResponse) => {
+export default async (request: NextApiRequest, response: NextApiResponse) => {
+  const listId = '1342c1d376'
+  const url = `https://us2.api.mailchimp.com/3.0/lists/${listId}/members`
   const { name, email } = request.body
-  const listId = ''
-  const leadData = {
+  const data = {
     email_address: email,
     status: 'subscribed',
+    tags: ['arquetipos'],
     full_name: name
   }
-  axios.post(`/lists/${listId}/members`, leadData, {
-    headers: {
-      'content-type': 'text/json'
-    }
-  })
-  return response.json(leadData)
+
+  try {
+    const result = await axios.post(url, data, {
+      headers: { Authorization: token }
+    })
+
+    return response.json(result.data)
+  } catch (error) {
+    console.error({ error: error.response.data.errors })
+
+    return response.json({ error: 'algo deu errado' })
+  }
 }
